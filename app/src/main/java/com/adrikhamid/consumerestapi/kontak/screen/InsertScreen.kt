0dcacb032f8325f26cbc5f34e786444948b8fd69
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2023-12-28 00:06:06 +0700

    add keyboardoptions in outlinetextfield nohp

--- app/src/main/java/com/adrikhamid/consumerestapi/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/adrikhamid/consumerestapi/kontak/screen/InsertScreen.kt
@@ -3,11 +3,13 @@ package com.adrikhamid.consumerestapi.kontak.screen
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.adrikhamid.consumerestapi.kontak.viewmodel.InsertUiEvent
 
@@ -42,6 +44,7 @@ fun FormInputSiswa(
         OutlinedTextField(
             value = insertUiEvent.nohp,
             onValueChange = { onValueChange(insertUiEvent.copy(nohp = it)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             label = { Text("Nomor HP") },
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
