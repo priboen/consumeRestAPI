56d6248c568d2a26b3fec18c918a53653362d9a2
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2023-12-28 00:00:03 +0700

    create fun Form Input Siswa and declare the parameter

---
+++ app/src/main/java/com/adrikhamid/consumerestapi/kontak/screen/InsertScreen.kt
@@ -0,0 +1,15 @@
+package com.adrikhamid.consumerestapi.kontak.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.adrikhamid.consumerestapi.kontak.viewmodel.InsertUiEvent
+
+@Composable
+fun FormInputSiswa(
+    insertUiEvent: InsertUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertUiEvent) -> Unit = {},
+    enabled: Boolean = true
+) {
+
+}
