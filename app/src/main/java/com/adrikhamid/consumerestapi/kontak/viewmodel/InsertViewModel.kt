b86d3aca4294bba6cd7875663895acc4181b25a5
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2023-12-28 00:43:27 +0700

    menambahkan yang kurang dan menyelesaikan InsertScreen.kt

--- app/src/main/java/com/adrikhamid/consumerestapi/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/adrikhamid/consumerestapi/kontak/viewmodel/InsertViewModel.kt
@@ -1,10 +1,31 @@
 package com.adrikhamid.consumerestapi.kontak.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.adrikhamid.consumerestapi.model.Kontak
 import com.adrikhamid.consumerestapi.repository.KontakRepository
+import kotlinx.coroutines.launch
 
 class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertKontakState(insertUiEvent: InsertUiEvent) {
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertKontak() {
+        viewModelScope.launch {
+            try {
+                kontakRepository.insertKontak(insertKontakState.insertUiEvent.toKontak())
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+        }
+    }
 }
 
 data class InsertUiState(
