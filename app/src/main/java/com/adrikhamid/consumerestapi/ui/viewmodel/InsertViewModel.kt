4c5a9f862a9005f5a3bae1e52bb3c7dc6a718c01
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2023-12-27 23:47:00 +0700

    create data class InsertUiEvent

--- app/src/main/java/com/adrikhamid/consumerestapi/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/adrikhamid/consumerestapi/ui/viewmodel/InsertViewModel.kt
@@ -3,6 +3,12 @@ package com.adrikhamid.consumerestapi.ui.viewmodel
 import androidx.lifecycle.ViewModel
 import com.adrikhamid.consumerestapi.repository.KontakRepository
 
-class InsertViewModel (private val kontakRepository: KontakRepository):ViewModel(){
+class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
 }
 
+data class InsertUiEvent(
+    val id: Int = 0,
+    val nama: String = "",
+    val email: String = "",
+    val nohp: String = ""
+)
