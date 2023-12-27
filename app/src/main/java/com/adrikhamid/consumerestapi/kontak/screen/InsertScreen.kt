2eb47e74d5f7dd0906f3cba8ccf2f919fbe768ae
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2023-12-28 00:03:42 +0700

    add OutlinedTextField for nama

--- app/src/main/java/com/adrikhamid/consumerestapi/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/adrikhamid/consumerestapi/kontak/screen/InsertScreen.kt
@@ -1,9 +1,17 @@
 package com.adrikhamid.consumerestapi.kontak.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.adrikhamid.consumerestapi.kontak.viewmodel.InsertUiEvent
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormInputSiswa(
     insertUiEvent: InsertUiEvent,
@@ -11,5 +19,18 @@ fun FormInputSiswa(
     onValueChange: (InsertUiEvent) -> Unit = {},
     enabled: Boolean = true
 ) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        OutlinedTextField(
+            value = insertUiEvent.nama,
+            onValueChange = { onValueChange(insertUiEvent.copy(nama = it)) },
+            label = { Text("Nama") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
 
+    }
 }
