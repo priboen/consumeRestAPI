a77a1b5cafe99e9e4b104dd1227d0d19fb1eb1e6
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2023-12-28 00:55:19 +0700

    change to PengelolaHalaman

--- app/src/main/java/com/adrikhamid/consumerestapi/ui/KontakApp.kt
+++ app/src/main/java/com/adrikhamid/consumerestapi/ui/KontakApp.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.adrikhamid.consumerestapi.R
+import com.adrikhamid.consumerestapi.navigation.PengelolaHalaman
 import com.adrikhamid.consumerestapi.ui.home.screen.HomeScreen
 import com.adrikhamid.consumerestapi.ui.home.viewmodel.HomeViewModel
 
@@ -63,12 +64,7 @@ fun KontakApp(
                 .fillMaxSize()
                 .padding(it)
         ) {
-            HomeScreen(
-                kontakUIState = homeViewModel.kontakUIState,
-                retryAction = homeViewModel::getKontak
-            )
-
-
+            PengelolaHalaman()
         }
     }
 }
