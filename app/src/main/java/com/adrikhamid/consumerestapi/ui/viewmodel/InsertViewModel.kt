8cf157a1580383dd70f7e0bfe20a992721700773
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2023-12-27 23:43:56 +0700

    create class InsertViewModel

---
+++ app/src/main/java/com/adrikhamid/consumerestapi/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,8 @@
+package com.adrikhamid.consumerestapi.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.adrikhamid.consumerestapi.repository.KontakRepository
+
+class InsertViewModel (private val kontakRepository: KontakRepository):ViewModel(){
+}
+
